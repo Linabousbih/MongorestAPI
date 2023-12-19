0f53a3b4a2905323695311a0f446a3bd5c2734c4
Linabousbih <linabousbih@gmail.com>
2023-12-19 21:14:58 +0100

    get method

---
+++ controllers/user.go
@@ -0,0 +1,47 @@
+package controllers
+
+import(
+	"fmt"
+	"encoding/json"
+	"mongorestapi/models"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"net/http"
+)
+
+type UserController struct{
+	session *mgo.Session
+}
+
+func NewUserController( s*mgo.Session) *UserController{
+	return &UserController{s}
+}
+
+func (uc UserController)GetUser(w http.ResponseWriter, r*http.Request, p httprouter.Param){
+	id :=p.ByName("id")
+	
+
+	if !bson.IsObjectIdHex(id){
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	oid:= bson.ObjectHex(id)
+
+	u:=models.User{}
+
+	if err:=uc.Session.DB("mongo.golang").C("users").FindId(oid).One(&u);err !=nil{
+		w.WriteHeader(404)
+		return
+	}
+
+	uj,err:=json.Marshal(u)
+	if err!=nil{
+		fmt.Print(err)
+	}
+
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+
+	fmt.Fprintf(w,"%s\n",uj)
+}
